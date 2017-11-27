cfaaffd98e296c491315ec4dcd7849987d6db3d3
lee-h-b <30491336+lee-h-b@users.noreply.github.com>
2017-11-27 21:56:07 +0900

    Add files via upload

---
+++ CharaSelect.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaSelect : MonoBehaviour {
+    public Transform spinModel;//도는 모델 단순히 메쉬를 바꾸는것부터 트랜스폼 자체를 대입받는식 2가지가 있겠음
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
